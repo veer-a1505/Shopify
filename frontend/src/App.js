e5e44228734832e239f9d9a5eb9ae3bc2f793b0c
veera_1505 <veerasaraha@gmail.com>
2021-01-30 22:13:44 +0530

    implementing the front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
 
 const App = () => {
-  return <div className="App">App</div>;
-};
+  return (
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route exact path='/' component={HomeScreen} />
+          <Route exact path='/products/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
+}
 
-export default App;
+export default App
