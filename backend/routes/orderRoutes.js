355460b68963d74e45f52a7a629907ad85fc0184
veera_1505 <veerasaraha@gmail.com>
2021-02-06 12:52:52 +0530

    implemented order controller and route in the  backend

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,9 @@
+import express from "express"
+const router = express.Router()
+import { addOrderItems } from "../controllers/orderControler.js"
+
+import { protect } from "../middleware/authMiddleware"
+
+router.route("/").post(protect, addOrderItems)
+
+export default router
